f985730a6338b09dcae778b7eecc739be101ccd9
ashish-greycube <mr.ashish.shah@gmail.com>
2018-04-11 19:13:54 +0530

    added js for get token

--- plaid_integration/hooks.py
+++ plaid_integration/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/plaid_integration/css/plaid_integration.css"
-# app_include_js = "/assets/js/plaid.desk.min.js"
+app_include_js = "/assets/js/plaid.desk.min.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/plaid_integration/css/plaid_integration.css"
