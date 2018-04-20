4a9f333c8f9eb6f83f7867d1e1f2958da830861c
ashish-greycube <mr.ashish.shah@gmail.com>
2018-04-20 11:54:53 +0530

    removed docs.py mess

--- plaid_integration/config/docs.py
+++ plaid_integration/config/docs.py
@@ -2,13 +2,10 @@
 Configuration for docs
 """
 
-source_link = "https://github.com/ashish-greycube/plaid_integration"
-docs_base_url = "https://ashish-greycube.github.io/plaid_integration"
-headline = "Integration between ERPNext journal entry and plaid transactions"
-sub_heading = "Documentation"
+# source_link = "https://github.com/ashish-greycube/plaid_integration"
+# docs_base_url = "https://ashish-greycube.github.io/plaid_integration"
+# headline = "Integration between ERPNext journal entry and plaid transactions"
+# sub_heading = "Documentation"
 
 def get_context(context):
 	context.brand_html = "Plaid Integration"
-    context.top_bar_items = [
-      {"label": "About", "url": context.docs_base_url + "/about"},
-    ]
